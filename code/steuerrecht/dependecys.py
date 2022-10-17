a4150565341554b12fe1b1c178d33f679f8c63a6
Ruben Kraus <s80350@htw-dresden.de>
2022-10-17 09:24:14 +0200

    updated var names

--- code/steuerrecht/dependecys.py
+++ code/steuerrecht/dependecys.py
@@ -1,12 +1,11 @@
 from steuerrecht import earnings, spendings, numbers
 
 def generate_number_dep():
-    NUMBERS = {}
+    numbers = {}
     for earn in earnings.ALL:
         for key, num in numbers.ALL.items():
             if earn is key:
-                NUMBERS[earn] = num
+                numbers[earn] = num
             else:
                 pass
-    print(NUMBERS)
     return NUMBERS
