0684e86366d20769cafd64a23505c14d28bccf9e
Ruben Kraus <s80350@htw-dresden.de>
2022-10-20 16:55:34 +0200

    next and previous as parameters for vertex

--- code/dag/vertex.py
+++ code/dag/vertex.py
@@ -3,12 +3,20 @@ class Vertex:
         self.id = node
         self.value = value
         self.adjacent = {}
+        self.next = {}
+        self.previous = {}
 
     def __str__(self):
         return str(self.id) + ' ajdacent: ' + str([x.id for x in self.adjacent])
 
     def add_neighbour(self, neighbour, weight=0):
         self.adjacent[neighbour] = weight
+
+    def add_next(self, next, weight=0):
+        self.next[next] = weight
+
+    def add_previous(self, previous, weight=0):
+        self.previous[previous] = weight
 
     def get_connected(self):
         return self.adjacent.keys()
