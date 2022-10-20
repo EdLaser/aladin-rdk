e2a8c7e6fe81e19bd25a1505204a6b1800453742
Ruben Kraus <s80350@htw-dresden.de>
2022-10-20 20:50:25 +0200

    added new graph layout with networkx

--- code/generate-tasks.py
+++ code/generate-tasks.py
@@ -1,54 +1,41 @@
 import random
 import string
+import networkx as nx
+import matplotlib.pyplot as plt
 from dag.graph import Graph
 from dag.vertex import Vertex
 from library import earnings
 from library import numbers
 from library import dependencys
-import PySimpleGUI as sg
 
 
 def setup_graph():
-    g = Graph()
-    g.add_vertex("start", "")
+    g = nx.DiGraph()
+    g.add_node("start", value="")
 
     for key_comb, value_comb in dependencys.COMBINATIONS.items():
-        print(g.add_vertex(key_comb, value_comb))
+        g.add_node(node_for_adding=key_comb, value=value_comb)
         g.add_edge("start", key_comb)
-
+        
         for key_earn, value_earn in earnings.ALL.items():
-            print(g.add_vertex(key_earn, value_earn))
-            g.add_edge(key_comb, key_earn)
-
-            for key_num, value_num in numbers.ALL.items():
-                print(g.add_vertex(key_num, value_num))
-                g.add_edge(key_earn, key_num)
+            g.add_node(node_for_adding= f"{key_comb}.{key_earn}", value=value_earn, nodetype=key_earn)
+            g.add_edge(key_comb, f"{key_comb}.{key_earn}")
+        
+            for value_num in numbers.ALL.items():
+                g.add_node(node_for_adding=f"{key_comb}.{key_earn}.{value_num}", value=value_num)
+                g.add_edge(f"{key_comb}.{key_earn}", f"{key_comb}.{key_earn}.{value_num}")
 
     print("\n")
     
-    sg.theme('LightBlue')
-    layout = [
-            [sg.Graph(canvas_size=(700,700), graph_bottom_left=(0, 0), graph_top_right=(700, 700), key='graph')]
-            ]
-    window = sg.Window('Created Graph', layout, finalize=True)
-    graph = window['graph']
-    pos_x = 40
-    pos_y = 660
-    
-    for vert in g:
-        print(vert)
-        graph.DrawCircle((pos_x, pos_y), 25)
-        pos_x += 35
-        pos_y -= 35
-    
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED:
-            break 
-    return window
+    print(g.nodes)
+    print(g.edges)
+    pos = nx.spring_layout(g)
+    plt.figure()
+    nx.draw_planar(g, arrows=True)
+    plt.savefig("graph.png", format="PNG")
+    plt.clf()
 
 
 if __name__ == '__main__':
     char = random.choice(string.ascii_letters).upper()
-    w = setup_graph()
-    w.close()
+    setup_graph()
--- code/library/earnings.py
+++ code/library/earnings.py
@@ -1,15 +1,13 @@
-from library import dependencys
-from library import sentenceparts
 import random
 
-GEHALT = {1: "verdient als Mitarbieter",
-          2: "erhält als Geschäftsführer ein Gehalt von",
-          3: "bekommt pro Monat"}
+GEHALT = ["verdient als Mitarbieter",
+          "erhält als Geschäftsführer ein Gehalt von",
+          "bekommt pro Monat"]
 
-BETEILIGUNG = {1: "erhält durch Beteiligung"}
+BETEILIGUNG = ["erhält durch Beteiligung"]
 
-DIVIDENDE = {1: "bekommt eine Dividende i.H.v"}
+DIVIDENDE = ["bekommt eine Dividende i.H.v"]
 
-VERMIETUNG = {1: "vermietet eine Wohnung, er verlangt"}
+VERMIETUNG = ["vermietet eine Wohnung, er verlangt"]
 
 ALL = {"GEHALT": GEHALT, "BETEILIGUNG": BETEILIGUNG, "DIVIDENDE": DIVIDENDE, "VERMIETUNG": VERMIETUNG}
