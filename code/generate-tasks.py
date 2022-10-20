c617b0c8b3bf527aebb7f04e7097e9ae7f71bb21
Ruben Kraus <s80350@htw-dresden.de>
2022-10-20 17:04:41 +0200

    basic graph generation

--- code/generate-tasks.py
+++ code/generate-tasks.py
@@ -1,10 +1,29 @@
 import random
-import earnings
-import numbers
 import string
+from dag.graph import Graph
+from dag.vertex import Vertex
+from library import earnings
+from library import numbers
+from library import dependencys
+
+
+def setup_graph():
+    g = Graph()
+    g.add_vertex("start", "")
+
+    for key_comb, value_comb in dependencys.COMBINATIONS.items():
+        print(g.add_vertex(key_comb, value_comb))
+        g.add_edge("start", key_comb)
+
+        for key_earn, value_earn in earnings.ALL.items():
+            print(g.add_vertex(key_earn, value_earn))
+            g.add_edge(key_comb, key_earn)
+
+    print("\n")
+    for vert in g:
+        print(vert)
+
 
 if __name__ == '__main__':
     char = random.choice(string.ascii_letters).upper()
-    task = earnings.set_params(char)
-
-    print(task)
+    setup_graph()
