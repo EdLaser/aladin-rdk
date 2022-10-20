24297ca1f543021e071d71c6c17ac70a11630485
Ruben Kraus <s80350@htw-dresden.de>
2022-10-20 17:07:39 +0200

    deleted unused method

--- code/library/dependencys.py
+++ code/library/dependencys.py
@@ -5,14 +5,3 @@ from library import numbers
 COMBINATIONS = {"COMB_1": ["Subjekt", "Verb", "Object"],
                 "COMB_2": ["Verb", "Subject", "Object"],
                 "COMB_3": ["Subject", "Object", "Verb"]}
-
-
-def generate_number_dep():
-    NUMBERS = {}
-    for earn in earnings.ALL:
-        for key, num in numbers.ALL.items():
-            if earn is key:
-                NUMBERS[earn] = num
-            else:
-                pass
-    return NUMBERS
--- code/library/earnings.py
+++ code/library/earnings.py
@@ -13,11 +13,3 @@ DIVIDENDE = {1: "bekommt eine Dividende i.H.v"}
 VERMIETUNG = {1: "vermietet eine Wohnung, er verlangt"}
 
 ALL = {"GEHALT": GEHALT, "BETEILIGUNG": BETEILIGUNG, "DIVIDENDE": DIVIDENDE, "VERMIETUNG": VERMIETUNG}
-
-
-# def set_params(char):
-#     arr = ""
-#     numbers = dependencys.generate_number_dep()
-#     for key, value in ALL.items():
-#         arr += f"{char} {random.choice(value)} {numbers[key]}. "
-#     return arr
