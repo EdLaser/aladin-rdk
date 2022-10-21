6aa40871160add7f176d4ba6e71038dd04a75645
Ruben Kraus <s80350@htw-dresden.de>
2022-10-21 17:37:08 +0200

    new methods to generate a dict to iterarte through

--- code/library/dependencys.py
+++ code/library/dependencys.py
@@ -1,7 +1,51 @@
+import imp
 from library import earnings
 from library import spendings
-from library import numbers
+from library import sentenceparts as sen
+import random
+import string
 
-COMBINATIONS = {"COMB_1": ["Subjekt", "Verb", "Object"],
+COMBINATIONS = {"COMB_1": ["Subject", "Verb", "Object"],
                 "COMB_2": ["Verb", "Subject", "Object"],
-                "COMB_3": ["Subject", "Object", "Verb"]}
+                # "COMB_3": ["Subject", "Object", "Verb"]
+                }
+
+def map_parts():
+    mapped = {}
+    for k, v in COMBINATIONS.items():
+        mapped[k] = generate_combs(sen.ALL, v, sen.VERBS, random.choice(string.ascii_letters).upper())
+    
+    return mapped
+            
+
+def generate_combs(formulation, sent_parts, verbs, obj):
+    combs = {}
+    for elem, value in formulation.items():
+        part_dict = {}
+        print(value)
+        for val in sent_parts:
+            if val == 'Subject':
+                part_dict[val] = random.choice(value)
+            elif val == 'Verb':
+                part_dict[val] = random.choice(verbs)
+            elif val == 'Object':
+                part_dict[val] = obj
+        combs[elem] = part_dict
+    
+    return combs
+
+
+TEST = {
+    'COMB_1': {
+        'G': {'S': 'test', 'O': 'test'},
+        'V': {'S': 'test', 'O': 'test'},
+        'D': {'S': 'test', 'O': 'test'},
+        'B': {'S': 'test', 'O': 'test'},
+    },
+    'COMB_2': {
+        'G': {'S': 'test', 'O': 'test'},
+        'V': {'S': 'test', 'O': 'test'},
+        'D': {'S': 'test', 'O': 'test'},
+        'B': {'S': 'test', 'O': 'test'},
+    }
+}
