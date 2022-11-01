11962897250ce5807fd23b3b296af09b80693aba
Ruben Kraus <s80350@htw-dresden.de>
2022-11-01 18:13:23 +0100

    function to build sentence

--- code/generate-tasks.py
+++ code/generate-tasks.py
@@ -1,4 +1,4 @@
-import string
+import random
 import pprint
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ def setup_graph():
     g = nx.DiGraph()
     g.add_node("start", value="", depth=0)
     combos = dependencys.map_parts()
-    pprint.pprint(combos)
+    # pprint.pprint(combos)
     for earning_key, earning_type in combos.items():
         g.add_node(node_for_adding=earning_key, value=earning_type, depth=1)
         g.add_edge("start", earning_key)
@@ -19,19 +19,24 @@ def setup_graph():
         #         node_for_adding=f"{earning_key}.{key}", value=phrase, depth=2)
         #     g.add_edge(earning_key, f"{earning_key}.{key}")
 
-    print("\n")
-
     # for elem in g:
     #     print(f"Node: {elem}\n---\tValue: {g.nodes[elem]}\n")
 
     return g
 
 
+def build_sent(parts: dict):
+    print(
+        f"{parts.get('Subject')} {parts.get('Verb')} {parts.get('Object')} {parts.get('Number')}")
+
+
 def traverse(difficculty: int, graph):
     # Traversiere immer wieder mit einer zufälligen Kombination
-    pprint.pprint(list(graph.nodes(data='value')))
+    nodes = list(graph.nodes(data='value'))
+    # pprint.pprint(nodes)
     for i in range(difficculty):
-        pass
+        node_value = random.choice(nodes)[1]
+        build_sent(node_value)
 
 
 if __name__ == '__main__':
