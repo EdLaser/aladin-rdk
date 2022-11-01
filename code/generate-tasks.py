9828dd45336525f4470eaf9ba913b57ee27d5a45
Ruben Kraus <s80350@htw-dresden.de>
2022-11-01 17:58:57 +0100

    formated

--- code/generate-tasks.py
+++ code/generate-tasks.py
@@ -13,27 +13,31 @@ def setup_graph():
     for earning_key, earning_type in combos.items():
         g.add_node(node_for_adding=earning_key, value=earning_type, depth=1)
         g.add_edge("start", earning_key)
-        
+
         for key, phrase in earning_type.items():
-            g.add_node(node_for_adding= f"{earning_key}.{key}", value=phrase, depth=2)
+            g.add_node(
+                node_for_adding=f"{earning_key}.{key}", value=phrase, depth=2)
             g.add_edge(earning_key, f"{earning_key}.{key}")
 
     print("\n")
-    
+
     # for elem in g:
-    #     print(f"Node: {elem}\n---\tValue: {g.nodes[elem]}\n")   
-    
+    #     print(f"Node: {elem}\n---\tValue: {g.nodes[elem]}\n")
+
     return g
+
 
 def traverse(difficculty: int, graph):
     # Traversiere immer wieder mit einer zufälligen Kombination
     for i in range(difficculty):
-        successors = graph.successors()
+
+        print(list(graph.nodes(data=True)))
+
 
 if __name__ == '__main__':
     # char = random.choice(string.ascii_letters).upper()
     g = setup_graph()
-   
+
     pos = nx.spring_layout(g)
     plt.figure()
     nx.draw_planar(g, arrows=True)
