4b44cd93d1904af8b06fdecac4699602b633e354
Ruben Kraus <s80350@htw-dresden.de>
2022-11-03 17:13:50 +0100

    type hinting

--- code/library/nodepool/nodepool.py
+++ code/library/nodepool/nodepool.py
@@ -1,20 +1,21 @@
 import random
+from library.nodepool.case import Case
 
 class NodePool:
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
         self.nodes = []
 
-    def show_nodes(self) -> list:
+    def show_nodes(self) -> list[Case]:
         return self.nodes
 
-    def add_node(self, node) -> list:
-        self.nodes += node
+    def add_node(self, node: Case) -> list[Case]:
+        self.nodes.append(node)
         return self.show_nodes()
 
-    def remove_node(self, node) -> list:
+    def remove_node(self, node: Case) -> list[Case]:
         self.nodes.remove(node)
         return self.show_nodes()
 
-    def pick_random_node(self):
+    def pick_random_node(self) -> Case:
         return random.choice(self.nodes)
