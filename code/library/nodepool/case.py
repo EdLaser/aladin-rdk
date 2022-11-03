e8b57ba7ed6ba8ee31340cfc7dc4bb6b2f9f2373
Ruben Kraus <s80350@htw-dresden.de>
2022-11-03 16:48:24 +0100

    updated docstrings

--- code/library/nodepool/case.py
+++ code/library/nodepool/case.py
@@ -12,21 +12,38 @@ class Case:
             Object: {self.object}\n"
 
     def set_name(self, name: str) -> str:
-        '''Set name of the case'''
+        '''
+        Set name of the case.
+
+            Returns:
+                self.name (str): Name of the case.
+        '''
         self.name = name
         return self.name
 
     def set_subject(self, subject: str) -> str:
-        '''Set subject of the case'''
+        '''Set subject of the case.
+
+            Returns:
+                    self.subject (str): Subject of the case.
+        '''
         self.subject = subject
         return self.subject
 
     def set_verb(self, verb: str) -> str:
-        '''Set verb of the case'''
+        '''Set verb of the case.
+                
+            Returns:
+                    self.verb (str): Verb of the case.
+        '''
         self.verb = verb
         return self.verb
 
     def set_object(self, object: str) -> str:
-        '''Set object of the case'''
+        '''Set object of the case.
+                
+            Returns:
+                    self.object (str): Object of the case.
+        '''
         self.object = object
         return self.object
