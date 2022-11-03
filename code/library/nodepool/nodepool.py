717f0e8d40e91bc2f075c2e045e6a44d7924ef8f
Ruben Kraus <s80350@htw-dresden.de>
2022-11-03 17:14:36 +0100

    more docstrings

--- code/library/nodepool/nodepool.py
+++ code/library/nodepool/nodepool.py
@@ -7,15 +7,19 @@ class NodePool:
         self.nodes = []
 
     def show_nodes(self) -> list[Case]:
+        '''Show all nodes of the pool.'''
         return self.nodes
 
     def add_node(self, node: Case) -> list[Case]:
+        '''Add a node to the pool.'''
         self.nodes.append(node)
         return self.show_nodes()
 
     def remove_node(self, node: Case) -> list[Case]:
+        '''Remove a node from the pool.'''
         self.nodes.remove(node)
         return self.show_nodes()
 
     def pick_random_node(self) -> Case:
+        '''Pick a random node out of the pool.'''
         return random.choice(self.nodes)
