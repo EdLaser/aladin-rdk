08621d126069c2adc66035d6683a82b8953c9df6
Ruben Kraus <s80350@htw-dresden.de>
2022-11-06 14:16:08 +0100

    wk number

--- code/library/numbers.py
+++ code/library/numbers.py
@@ -13,5 +13,8 @@ DIVIDENDE = round_to_base(random.randrange(50000, 100000), 1000)
 
 VERMIETUNG = round_to_base(random.randrange(500, 2000), 50)
 
+WERBUNGSKOSTEN = round_to_base(random.randrange(100, 2000), 50)
+
 ALL = {"Gehalt": GEHALT, "Beteiligung": BETEILIGUNG,
-       "Dividende": DIVIDENDE, "Vermietung": VERMIETUNG}
+       "Dividende": DIVIDENDE, "Vermietung": VERMIETUNG,
+       "Werbungskosten": WERBUNGSKOSTEN}
