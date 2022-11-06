362ecbf4b4094b431592d14ce57d60fb4a62aab7
Ruben Kraus <s80350@htw-dresden.de>
2022-11-06 19:26:57 +0100

    generate tasks and return template

--- code/recht/routes.py
+++ code/recht/routes.py
@@ -1,8 +1,17 @@
-from flask import jsonify, make_response, render_template, request, Blueprint
-
+from flask import render_template, request, Blueprint
+from . import generate_tasks as gen
 
 bp = Blueprint('routes', __name__)
 
+
 @bp.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    all_cases = gen.generate()
+    print(all_cases)
+    if request.method == 'GET':
+        return render_template('index.html', li=all_cases)
+
+    if request.method == 'POST':
+        return render_template('index.html', li=all_cases)
+    else:
+        return render_template('index.html')
