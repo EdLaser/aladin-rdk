e1c824d375bfce70778b4a66d226cec371b2a421
Ruben Kraus <s80350@htw-dresden.de>
2022-11-07 14:26:12 +0100

    set list as debug statement

--- code/recht/routes.py
+++ code/recht/routes.py
@@ -7,12 +7,11 @@ bp = Blueprint('routes', __name__)
 @bp.route("/", methods=['GET', 'POST'])
 def index():
     all_cases = gen.generate()
-    ai_test = gen.call_ai()
     print(all_cases)
     if request.method == 'GET':
-        return render_template('index.html', li=all_cases, test=ai_test)
+        return render_template('index.html', li=all_cases, test=all_cases)
 
     if request.method == 'POST':
-        return render_template('index.html', li=all_cases, test=ai_test)
+        return render_template('index.html', li=all_cases, test=all_cases)
     else:
         return render_template('index.html')
