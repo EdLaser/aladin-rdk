bcc9d7ec471832a49f75b373b98326fc6e5679b7
Ruben Kraus <s80350@htw-dresden.de>
2022-11-08 09:33:39 +0100

    changed show nodes method

--- code/recht/library/nodepool/nodepool.py
+++ code/recht/library/nodepool/nodepool.py
@@ -9,16 +9,20 @@ class NodePool:
     def __iter__(self):
         return iter(self.nodes)
 
-    def show_nodes(self) -> list[Case]:
+    def show_nodes(self) -> list[str]:
         '''Show all nodes of the pool.'''
-        return self.nodes
+        nodes_as_string = []
+        for c in self.nodes:
+            nodes_as_string.append(str(c))
+        return nodes_as_string
+        
 
-    def add_node(self, node: Case) -> list[Case]:
+    def add_node(self, node: Case) -> list[str]:
         '''Add a node to the pool.'''
         self.nodes.append(node)
         return self.show_nodes()
 
-    def remove_node(self, node: Case) -> list[Case]:
+    def remove_node(self, node: Case) -> list[str]:
         '''Remove a node from the pool.'''
         self.nodes.remove(node)
         return self.show_nodes()
