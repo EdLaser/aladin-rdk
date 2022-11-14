bbcd4df97a4296ec8cd0d5a87c6bcad7ed536d27
Ruben Kraus <s80350@htw-dresden.de>
2022-11-14 15:34:33 +0100

    changed paragraph symbol location

--- code/recht/library/laws.py
+++ code/recht/library/laws.py
@@ -1,7 +1,7 @@
-NICHT_SELBSTSTÄNDIG = {'19 Abs. 1 Nr.1': ['Gehalt']}
+NICHT_SELBSTSTÄNDIG = {'§19 Abs. 1 Nr.1': ['Gehalt']}
 
-VERMIETUNG = {'21 Abs.1 Nr. 1': ['Vermietung']}
+VERMIETUNG = {'§21 Abs.1 Nr. 1': ['Vermietung']}
 
-KAPITALVERMOEGEN = {'20 Abs.1 Nr. 1': ['Beteiligung', 'Dividende']}
+KAPITALVERMOEGEN = {'§20 Abs.1 Nr. 1': ['Beteiligung', 'Dividende']}
 
-WERBUNGSKOSTEN = {'9 Abs 1.' : ['Gehalt-WK', 'Vermietung-WK', 'Abschreibung'], '20 Abs. 9': ['Dividende', 'Beteiligung']}
+WERBUNGSKOSTEN = {'§9 Abs 1.' : ['Gehalt-WK', 'Vermietung-WK', 'Abschreibung'], '§20 Abs. 9': ['Dividende', 'Beteiligung']}
