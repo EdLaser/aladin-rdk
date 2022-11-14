de946d6ef2b7731982845a79d2f1aebfff7a1ca0
Ruben Kraus <s80350@htw-dresden.de>
2022-11-14 10:46:31 +0100

    added laws based fot the cases

---
+++ code/recht/library/laws.py
@@ -0,0 +1,7 @@
+NICHT_SELBSTSTÄNDIG = {'19 Abs. 1 Nr.1': ['Gehalt']}
+
+VERMIETUNG = {'21 Abs.1 Nr. 1': ['Vermietung']}
+
+KAPITALVERMOEGEN = {'20 Abs.1 Nr. 1': ['Beteiligung', 'Dividende']}
+
+WERBUNGSKOSTEN = {'9 Abs 1.' : ['Gehalt', 'Vermietung'], '20 Abs. 9': ['Dividende', 'Beteiligung']}
