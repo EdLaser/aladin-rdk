b34f67d2f59723ced731279aa0ae7bb4e3398b5d
Ruben Kraus <s80350@htw-dresden.de>
2022-11-14 13:26:07 +0100

    added Abschreibung to law

--- code/recht/library/laws.py
+++ code/recht/library/laws.py
@@ -4,4 +4,4 @@ VERMIETUNG = {'21 Abs.1 Nr. 1': ['Vermietung']}
 
 KAPITALVERMOEGEN = {'20 Abs.1 Nr. 1': ['Beteiligung', 'Dividende']}
 
-WERBUNGSKOSTEN = {'9 Abs 1.' : ['Gehalt', 'Vermietung'], '20 Abs. 9': ['Dividende', 'Beteiligung']}
+WERBUNGSKOSTEN = {'9 Abs 1.' : ['Gehalt-WK', 'Vermietung-WK', 'Abschreibung'], '20 Abs. 9': ['Dividende', 'Beteiligung']}
