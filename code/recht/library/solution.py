0c2a9d145020dd412d5a02343520cc1524c561b4
Ruben Kraus <s80350@htw-dresden.de>
2022-11-16 12:14:20 +0100

    added type of case

--- code/recht/library/solution.py
+++ code/recht/library/solution.py
@@ -1,8 +1,9 @@
 class Solution:
-    def __init__(self,case_name="", law="", number=0, hint="") -> None:
+    def __init__(self,case_name="", law="", number=0, type_of_case="", hint="") -> None:
         self.case_name = case_name
         self.law = law
         self.number = number
+        self.type_of_case = type_of_case
         self.hint = hint
 
     def __str__(self) -> str:
