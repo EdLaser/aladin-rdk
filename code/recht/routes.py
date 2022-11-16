0b64e513d6826e80069a11752e9e535df355f509
Ruben Kraus <s80350@htw-dresden.de>
2022-11-16 12:55:48 +0100

    deleted newline

--- code/recht/routes.py
+++ code/recht/routes.py
@@ -10,7 +10,6 @@ def index():
     print(f"Pool_list: {all_cases['pool']}")
     if request.method == 'GET':
         return render_template('index.html', li=all_cases['li'], sol=all_cases['solution'], sum=all_cases['sum'])
-
     if request.method == 'POST':
         return render_template('index.html', li=all_cases['li'], sol=all_cases['solution'], sum=all_cases['sum'])
     else:
