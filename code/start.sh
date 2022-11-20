bd6d5dffe0d47d06a768ce0caea9e095188a7b93
Ruben Kraus <s80350@htw-dresden.de>
2022-11-20 16:16:40 +0100

    added debug mode

--- code/start.sh
+++ code/start.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH="${PYTHONPATH}:/Users/ruben/dev/aladin-rdk/code/recht"
-python3 -m flask --app=recht run --host=0.0.0.0 --port=8000
+python3 -m flask --app=recht --debug run --host=0.0.0.0 --port=8000
