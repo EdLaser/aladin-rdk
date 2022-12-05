79ca47c3d0b2c1b1480d2e2715fe76fb9e545258
Ruben Kraus <s80350@htw-dresden.de>
2022-12-05 10:22:52 +0100

    added to dict method

--- code/recht/library/solution.py
+++ code/recht/library/solution.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 class Solution:
     def __init__(self,case_name="", law="", number=0, type_of_case="", hint="") -> None:
         self.case_name = case_name
@@ -8,3 +10,6 @@ class Solution:
 
     def __str__(self) -> str:
         return f"Casename: {self.case_name} | Law: {self.law} | Amount: {self.number}."
+
+    def to_dict(self) -> Dict[str, str|int]:
+        return {"case_name": self.case_name, "law": self.law, "number": self.number, "type_of_case": self.type_of_case, "hint": self.hint}
