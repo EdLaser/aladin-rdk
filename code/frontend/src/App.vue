6f3be1e367fbfad092e37c321998781250cc6697
Ruben Kraus <s80350@htw-dresden.de>
2022-12-17 18:11:17 +0100

    basic vue setup

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -3,8 +3,29 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <RouterView />
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+                <h3 class="float-md-start mb-0">
+                    Steuerrecht Aufgaben
+                </h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link fw-bold py-1 px-0 active" href="https://github.com/EdLaser/aladin-rdk">
+                        Aladin - Steuerrecht
+                    </a>
+                </nav>
+            </div>
+        </header>
+        <main class="px-3">
+            <h1>Generierung einer Aufgabe</h1>
+            <p class="lead">
+                Hier werden Aufgaben zum Steuerrecht generiert!
+            </p>
+            <div class="container w-100 mb-5">
+                <div class="row justify-content-center">
+                    
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
