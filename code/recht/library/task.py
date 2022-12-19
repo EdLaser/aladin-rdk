d67dca7f062510e518d17b08c82097e2a23a7fe8
Ruben Kraus <s80350@htw-dresden.de>
2022-12-19 18:05:58 +0100

    added type hinz

--- code/recht/library/task.py
+++ code/recht/library/task.py
@@ -1,12 +1,12 @@
 from library.nodepool.case import Case
 import itertools
 
-class Task():
+class Task:
     id_generated = itertools.count()
 
     def __init__(self, cases: list[Case]=[], solved: bool=False) -> None:
         self.id = next(Task.id_generated)
-        self.cases = cases
+        self.cases: list[Case] = cases
         self.solved = solved
 
     def to_dict(self):
