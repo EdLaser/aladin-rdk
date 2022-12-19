2208a6bbb941b558650b24e774d5af62f70ecd3e
Ruben Kraus <s80350@htw-dresden.de>
2022-12-19 18:05:00 +0100

    added getter and setter and missing return statement

--- code/recht/generator_strategie.py
+++ code/recht/generator_strategie.py
@@ -4,28 +4,33 @@ from library.nodepool.case import Case
 
 class GeneratorStrategie(ABC):
     @abstractmethod
-    def generate(self, parameters) -> list[Case]:
+    def generate(self, parameters) :
         pass
 
 class WithDifficultyAndNeededAndAmount(GeneratorStrategie):
-    def generate(self, parameters) -> list[Case]:
+    def generate(self, parameters) :
         return gen.generate(difficulty=int(parameters.get('difficulty')), amount=int(parameters.get('amount')), needed=parameters.get('needed'))
 
 class WithDifficultyAndAmount(GeneratorStrategie):
-    def generate(self, parameters) -> list[Case]:
+    def generate(self, parameters):
         return gen.generate(difficulty=int(parameters.get('difficulty')), amount=int(parameters.get('amount')))
 
 class Default(GeneratorStrategie):
-    def generate(self) -> list[Case]:
+    def generate(self) :
         return gen.generate()
 
 class Context:
     def __init__(self, strategy: GeneratorStrategie):
         self._strategy = strategy
 
-    def set_strategy(self, strategy: GeneratorStrategie):
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: GeneratorStrategie):
         self._strategy = strategy
 
     # The Context delegates the execution of the algorithm to the strategy object.
-    def generate_tasks(self, parameters) -> list[Case]:
-        self._strategy.generate(parameters)
+    def generate_tasks(self, parameters):
+        return self._strategy.generate(parameters)
