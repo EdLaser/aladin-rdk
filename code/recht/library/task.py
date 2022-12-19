80207114c85d0fa4a4da1b8aa7a39f6ad409a725
Ruben Kraus <s80350@htw-dresden.de>
2022-12-19 17:25:32 +0100

    new task class

---
+++ code/recht/library/task.py
@@ -0,0 +1,13 @@
+from nodepool.case import Case
+import itertools
+
+class Task():
+    id_generated = itertools.count()
+
+    def __init__(self, cases: list[Case]=[], solved: bool=False) -> None:
+        self.id = next(Task.id_generated)
+        self.cases = cases
+        self.solved = solved
+
+    def to_dict(self):
+        return {"id": self.id, "cases": [case.to_dict() for case in self.cases], "solved": self.solved}
