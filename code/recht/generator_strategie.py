83b51420e840f45e16561efe0b0719eff877b705
Ruben Kraus <s80350@htw-dresden.de>
2022-12-19 17:33:20 +0100

    added type conversion

--- code/recht/generator_strategie.py
+++ code/recht/generator_strategie.py
@@ -4,16 +4,16 @@ from library.nodepool.case import Case
 
 class GeneratorStrategie(ABC):
     @abstractmethod
-    def generate(self) -> list[Case]:
+    def generate(self, parameters) -> list[Case]:
         pass
 
 class WithDifficultyAndNeededAndAmount(GeneratorStrategie):
     def generate(self, parameters) -> list[Case]:
-        return gen.generate(difficulty=parameters.get('difficulty'), amount=parameters.get('amount'), needed=parameters.get('needed'))
+        return gen.generate(difficulty=int(parameters.get('difficulty')), amount=int(parameters.get('amount')), needed=parameters.get('needed'))
 
 class WithDifficultyAndAmount(GeneratorStrategie):
     def generate(self, parameters) -> list[Case]:
-        return gen.generate(difficulty=parameters.get('difficulty'), amount=parameters.get('amount'))
+        return gen.generate(difficulty=int(parameters.get('difficulty')), amount=int(parameters.get('amount')))
 
 class Default(GeneratorStrategie):
     def generate(self) -> list[Case]:
@@ -27,5 +27,5 @@ class Context:
         self._strategy = strategy
 
     # The Context delegates the execution of the algorithm to the strategy object.
-    def generate_tasks(self) -> list[Case]:
-        self._strategy.generate()
+    def generate_tasks(self, parameters) -> list[Case]:
+        self._strategy.generate(parameters)
