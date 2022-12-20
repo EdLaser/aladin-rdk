f593a0f54ee2bd63f1ed86597e2353e2ffc39492
Ruben Kraus <s80350@htw-dresden.de>
2022-12-20 18:08:36 +0100

    TaskComponent to display Tasks

---
+++ code/frontend/src/components/TaskSentenceComponent.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            task_id: null,
+            sentences: []
+        };
+    },
+    methods: {
+        getTask() {
+            const url = 'http://localhost:8000/get-task';
+            axios.get(url)
+                .then((res) => {
+                    this.sentences = res.data.sentences;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        this.getTask();
+    }
+}
+</script>
+<template>
+    <div class="container">
+        <p>
+            <template v-for="sentence in sentences">
+                {{ sentence }}
+            </template>
+        </p>
+    </div>
+</template>
