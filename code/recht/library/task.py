4fdd7ed94cd17d2be132d6f99b20d9bf681914e4
Ruben Kraus <s80350@htw-dresden.de>
2022-12-30 15:15:10 +0100

    added zve to task

--- code/recht/library/task.py
+++ code/recht/library/task.py
@@ -1,12 +1,14 @@
 from library.nodepool.case import Case
+from library.solution import Solution
 import itertools
 
 class Task:
     id_generated = itertools.count()
 
-    def __init__(self, cases: list[Case]=[], zve: int = 0, solved: bool=False) -> None:
+    def __init__(self, cases: list[Case]=[], zve: int = 0, solutions: list[Solution] = [], solved: bool=False) -> None:
         self.id = next(Task.id_generated)
         self.cases: list[Case] = cases
+        self.solutions = solutions
         self.zve = zve
         self.solved = solved
 
