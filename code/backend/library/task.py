ac02e734dbefe39853e137cf3f7c8451ef88f31d
Ruben Kraus <s80350@htw-dresden.de>
2023-01-08 11:40:17 +0100

    reformat

--- code/backend/library/task.py
+++ code/backend/library/task.py
@@ -5,7 +5,7 @@ import itertools
 class Task:
     id_generated = itertools.count()
 
-    def __init__(self, cases: list[Case]=[], zve: int = 0, solutions: list[Solution] = [], solved: bool=False) -> None:
+    def __init__(self, cases: list[Case] = [], zve: int = 0, solutions: list[Solution] = [], solved: bool=False) -> None:
         self.id = next(Task.id_generated)
         self.cases: list[Case] = cases
         self.solutions = solutions
