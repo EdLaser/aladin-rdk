ce9096ef5ddf92b7864c1c1e271eb0ac64f3fee7
Ruben Kraus <s80350@htw-dresden.de>
2023-01-08 12:10:39 +0100

    generate values of solved cases

--- code/backend/library/task.py
+++ code/backend/library/task.py
@@ -5,12 +5,12 @@ import itertools
 class Task:
     id_generated = itertools.count()
 
-    def __init__(self, cases: list[Case] = [], zve: int = 0, solutions: list[Solution] = [], solved: bool=False) -> None:
+    def __init__(self, cases: dict[int, Case] = {}, zve: int = 0, solutions: list[Solution] = []) -> None:
         self.id = next(Task.id_generated)
-        self.cases: list[Case] = cases
+        self.cases: dict[int, Case] = cases
         self.solutions = solutions
         self.zve = zve
-        self.solved = solved
+        self.solved = {case_id: False for case_id in self.cases.keys()}
 
     def to_dict(self):
-        return {"id": self.id, "cases": [case.to_dict() for case in self.cases], "solved": self.solved}
+        return {"id": self.id, "cases": [case.to_dict() for case in self.cases.values()], "solved": self.solved}
