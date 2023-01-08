8e826c34da87f6c57d4579fe70e1469fb24c4ab0
Ruben Kraus <s80350@htw-dresden.de>
2023-01-08 17:56:45 +0100

    generate solve on inti

--- code/backend/library/task.py
+++ code/backend/library/task.py
@@ -5,12 +5,13 @@ import itertools
 class Task:
     id_generated = itertools.count()
 
-    def __init__(self, cases: list[Case] = [], zve: int = 0, solutions: dict[int, Solution] = {}) -> None:
+    def __init__(self, cases: list[Case] = [], zve: int = 0, solutions: dict[str, Solution] = {}) -> None:
         self.id = next(Task.id_generated)
+        # replace as tupel instead of list
         self.cases: list[Case] = cases
-        self.solutions: dict[int, Solution] = solutions
+        self.solutions: dict[str, Solution] = solutions
         self.zve = zve
-        self.solved = {sol_id: False for sol_id in self.solutions.keys()}
+        self.solved = {sol_id: {'name': False, 'law': False, 'num': False} for sol_id in self.solutions.keys()}
 
     def to_dict(self):
         return {"id": self.id, "case": [case.to_dict() for case in self.cases], "solved": self.solved}
